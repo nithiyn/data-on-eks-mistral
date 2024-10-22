32201209af1ef8a71c92c477c327cf0d4c968703
nithiyn <vijeasns@amazon.com>
2024-10-22 11:06:21 -0400

    commit trn-var

--- ai-ml/trainium-inferentia/variables.tf
+++ ai-ml/trainium-inferentia/variables.tf
@@ -9,7 +9,7 @@ variable "name" {
 variable "region" {
   description = "region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-2"
 }
 
 variable "eks_cluster_version" {
